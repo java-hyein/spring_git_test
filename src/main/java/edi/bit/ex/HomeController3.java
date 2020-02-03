a619d285c697bf3993aa39e5176906fc43474bca
HYEIN LEE <java.hyein@gmail.com>
2020-02-03 09:58:27 +0900

    Revert "Revert "3번 추가""
    This reverts commit 329a9343bd8e63bfe358aab6d8888ca8af22f033.

--- src/main/java/edi/bit/ex/HomeController3.java
+++ src/main/java/edi/bit/ex/HomeController3.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController3 {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController3.class);
+	//3번 추가
 	//2번 추가
 	//1번 추가
 	/**
